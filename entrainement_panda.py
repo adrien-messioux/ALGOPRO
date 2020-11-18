4f7ba8513dd82c73d25700fbc304231f612223b9
adrien-messioux <72786466+adrien-messioux@users.noreply.github.com>
2020-11-18 09:28:32 +0100

    Add files via upload

---
+++ entrainement_panda.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 17 15:33:47 2020
+
+@author: Adri
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data=pd.read_excel("algopro.xls")
